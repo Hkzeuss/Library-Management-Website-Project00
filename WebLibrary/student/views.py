76b9a2f463a61cce3b6e40f82b08e1c2ab576730
manh0107 <phamquangmanh0107@gmail.com>
2023-11-02 09:14:20 +0700

    upload forgot password

--- WebLibrary/student/views.py
+++ WebLibrary/student/views.py
@@ -3,9 +3,14 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required, user_passes_test
-
+from django.core.mail import send_mail
+from .models import OTP
 from .forms import LoginForm
-
+import random
+from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_decode
+from django.urls import reverse
 
 def home(request):
     return render(request, 'student/home.html')
@@ -27,7 +32,8 @@ def loginPage(request):
                 login(request, user)
                 return redirect('homefanpage') 
             else:
-                form.add_error(None, "Bạn đã nhập sai tài khoản hoặc mật khẩu. Xin vui lòng thử lại")
+                form.add_error(None, "Bạn đã nhập sai tài khoản hoặc mật khẩu")
+                messages.error(request, "Xin vui lòng nhập lại")
 
     else:
         form = LoginForm()
@@ -97,11 +103,74 @@ def signout(request):
     return redirect('/')
 
 def forgot(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        username = request.POST.get("username")
+        
+        # Kiểm tra xem username và email đã được nhập hay chưa
+        if not username:
+            messages.error(request, "Tên người dùng không được bỏ trống.")
+            return redirect("forgot")
 
-    return render(request, 'student/ForgotPassword.html')
+        if not email:
+            messages.error(request, "Email không được bỏ trống.")
+            return redirect("forgot")
+
+        try:
+            user = User.objects.get(username=username, email=email)
+            otp = str(random.randint(100000, 999999))
+            OTP.objects.create(user=user, token=otp)
+            subject = "Tài khoản của bạn sẽ được đổi mật khẩu khi bạn nhập OTP"
+            message = f"OTP của bạn là: {otp}"
+            from_email = 'vjulib@gmail.com'
+            recipient_list = [email]
+            send_mail(subject, message, from_email, recipient_list)
+            return redirect("confirm")
+        except User.DoesNotExist:
+            messages.error(request, "Không tìm thấy tài khoản với thông tin bạn nhập.")
+            return redirect("forgot")
+    return render(request, "student/ForgotPassword.html")
 
 def confirm(request):
+    if request.method == "POST":
+        otp = request.POST.get("otp")
+        try:
+            reset_request = OTP.objects.get(token=otp)
+            if reset_request.token == otp:
+                return redirect('reset', token=reset_request.token)
+            else:
+                messages.error(request, "Mã OTP không hợp lệ.")
+        except OTP.DoesNotExist:
+            messages.error(request, "Mã OTP không hợp lệ.")
+    
     return render(request, 'student/ConfirmForgotPassword.html')
+
+
+def reset(request, token):
+    context={}
+    try:
+        OTP_user = OTP.objects.filter(token = token).first()
+
+        if request.method == "POST":
+            new_password = request.POST.get("new_password")
+            confirm_password = request.POST.get("confirm_password")
+            user_id = request.POST.get("user_id")
+            
+            if user_id is None:
+                messages.error(request, "Không tìm thấy thông tin người dùng")
+                return redirect('reset', token=token)
+            if new_password != confirm_password:
+                messages.error(request, "Xin vui lòng nhập lại")
+                return redirect('reset', token=token)
+        
+            User_obj = User.objects.get(id=user_id)
+            User_obj.set_password(new_password)
+            User_obj.save()
+            return redirect('success')
+        context = {'user_id': OTP_user.user.id}
+    except Exception as e:
+        print(e)
+    return render(request, 'student/ResetPassword.html', context)
 
 def success(request):
     return render(request, 'student/succsesforgotpassword.html')
