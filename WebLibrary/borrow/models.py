37ddaebd04175daa13f4240930ad55d0c7c5a008
yukioreki1302 <21110112@st.vju.ac.vn>
2023-11-13 22:46:36 +0700

    Update

--- WebLibrary/borrow/models.py
+++ WebLibrary/borrow/models.py
@@ -11,8 +11,10 @@ class Borrow(models.Model):
     is_returned = models.BooleanField(default=False)
 
     def __str__(self):
+        print(f'self.user: {self.user}')
         user_username = self.user.username if self.user else "username"
         return f'{user_username} mượn {self.book.title} từ {self.borrow_date} đến {self.return_date}'
+
 
     def get_absolute_url(self):
         return reverse('borrow:borrow-detail', args=[str(self.id)])
